19f9b74ffa9e9ec7d90dac73374c69f091a7b721
Abhimanyu Kumar Vatsa <itorian@live.in>
2016-02-02 07:43:28 +0530

    initial commit by abhimanyu

---
+++ WebFormApplication1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebFormApplication1.Startup))]
+namespace WebFormApplication1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ WebMVCApplication1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebMVCApplication1.MvcApplication" Language="C#" %>
