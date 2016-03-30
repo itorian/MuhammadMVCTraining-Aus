356418efdb145ea217033e589655a9b14afd5483
Abhimanyu Kumar Vatsa <itorian@live.in>
2016-03-30 07:39:43 +0530

    josn parsing in js and c#

--- JSONObjectHandling/App_Start/RouteConfig.cs
+++ JSONObjectHandling/App_Start/RouteConfig.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace WebAPIMVCClientReference
+namespace JSONObjectHandling
 {
     public class RouteConfig
     {
@@ -16,7 +16,7 @@ namespace WebAPIMVCClientReference
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Student", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ JSONObjectHandling/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.Mvc;
+
+namespace JSONObjectHandling.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string test)
+        {
+
+            // todo: insert in database
+
+            return View();
+        }
+
+        public string SomeOtherMethod(string json)
+        {
+            List<Admission> list = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Admission>>(json); // JSON.parse
+            Debug.WriteLine(list);
+            string reJson = Newtonsoft.Json.JsonConvert.SerializeObject(list); // JSON.stringify
+            Debug.WriteLine(reJson);
+            return "true";
+        }
+    }
+
+    public class Admission
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+}
